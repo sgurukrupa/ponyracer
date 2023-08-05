6121c72cab75405465779250e859fa8f627ce886
Ganesh A Hegde <sgurukrupa@gmail.com>
2023-08-05 12:17:06 +0530

    Completed exercise 3, Haribol!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MenuComponent } from './menu/menu.component';
+import { RacesComponent } from './races/races.component';
 
 @Component({
   selector: 'pr-root',
   standalone: true,
-  imports: [CommonModule, MenuComponent],
+  imports: [CommonModule, MenuComponent, RacesComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
